304c9007f9275af3905260a879a962ac9f5bee55
Aveen Ismail <aveen.ismail@yubico.com>
2024-04-24 13:40:09 +0200

    generate versioninfo_windows

--- versioninfo_windows.go
+++ versioninfo_windows.go
@@ -12,6 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:generate goversioninfo -64
+go:generate goversioninfo -64
 
 package main
